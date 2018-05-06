a8f2c0e72cba81a3aee365f89e83cc18024aa099
a7mad799 <a7mad.1.2008@gmail.com>
2018-05-06 18:51:51 +0300

    v 0.01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavItemComponent } from './nav-item/nav-item.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavItemComponent,
+    ParagraphComponent,
+    GalleryComponent,
+    NavComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
